4d893246e165515f1548885f179cd31ca7280cee
Olof Stacke <olof.stacke@consid.se>
2024-03-12 09:32:30 +0100

    now displays time of notification and plays a sound

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-extern crate notify_rust;
 use notify_rust::Notification;
 use std::thread;
 use std::time::Duration;
+use notify_rust::Timeout;
+use winapi::um::winuser::MessageBeep;
+use chrono::{Local, DateTime};
 
 fn main() {
     // Get the number of minutes to wait from command line arguments
@@ -23,10 +25,19 @@ fn main() {
     // Wait for the specified number of seconds
     println!("Waiting for {} minutes...", minutes);
     thread::sleep(Duration::from_secs(minutes * 60));
+    
+    // Get the current time in your local timezone
+    let local_time: DateTime<Local> = Local::now();
+    // Format the time as a string
+    let time_string = local_time.format("%H:%M").to_string();
 
     // Display the notification
     let _ = Notification::new()
         .summary("Notification")
-        .body(&format!("Time's up! Notification after {} minutes.", minutes))
+        .body(&format!("{}: Time's up! Notification after {} minutes.", time_string, minutes))
+        .timeout(Timeout::Never) // this however is
         .show();
+
+    // Play a sound
+    unsafe { MessageBeep(0xFFFFFFFF); } // Plays the default system beep sound
 }
